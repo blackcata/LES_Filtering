a5ac04abfc0c2b0f173208b640fd5d840af0272f
blackcata <blackcata109@gmail.com>
2016-12-07 11:01:15 +0900

    Add LES_filtering main fortran file

---
+++ LES_Filtering_main.f90
@@ -0,0 +1,18 @@
+!-----------------------------------------------------------------------------------!
+!
+!   PROGRAM : LES_Filtering_main.f90
+!
+!   PURPOSE : To LES-filter the DNS data for turbulent channel flow.
+!             (a) : obtain a filtered velocity field by applying Gaussian filter
+!             (b) : obtain the residual-stress tensor
+!             (c) : obtain the residual viscosity
+!             (d) : obtain averaged Smagorinsky coefficient 
+!
+!                                                             2016.12.07 K.Noh
+!
+!-----------------------------------------------------------------------------------!
+
+        PROGRAM LES_FILTERING
+          IMPLICIT NONE
+
+        END PROGRAM LES_FILTERING
